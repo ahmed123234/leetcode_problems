9ac90cbdc3dae08dcb5cd16104d3526678316679
ahmed <ahmadghnnam60@gmail.com>
2023-03-05 23:00:13 +0300

    added a JavaScript solution of Number of Valid Clock Times

---
+++ 2437.Number_of_Valid_Clock_Times/2437.Number_of_Valid_Clock_Times.js
@@ -0,0 +1,27 @@
+var countTime = function(time) {
+   
+    let hh = 1;
+    let mm = 1;
+		if (time.charAt(0) == '?' && time.charAt(1) != '?') {
+			if (time.charAt(1) <= '3') {
+				hh *= 3;
+			} else {
+				hh *= 2;
+			}
+		} else if (time.charAt(0) != '?' && time.charAt(1) == '?') {
+			if (time.charAt(0) <= '1') {
+				hh *= 10;
+			} else {
+				hh *= 4;
+			}
+		} else if (time.charAt(0) == '?' && time.charAt(1) == '?') {
+			hh *= 24;
+		}
+		if (time.charAt(3) == '?') {
+			mm *= 6;
+		}
+		if (time.charAt(4) == '?') {
+			mm *= 10;
+		}
+		return hh * mm;
+};
