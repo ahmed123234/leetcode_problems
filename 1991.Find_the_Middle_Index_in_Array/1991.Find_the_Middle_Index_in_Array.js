2b3d0ee6fad1a8e44493056c41b167b4f2bc2174
ahmed <ahmadghnnam60@gmail.com>
2023-03-07 06:33:50 +0300

    added a JavaScript solution of Find the Middle Index in Array

---
+++ 1991.Find_the_Middle_Index_in_Array/1991.Find_the_Middle_Index_in_Array.js
@@ -0,0 +1,24 @@
+var findMiddleIndex = function(nums) {
+    let n = nums.length;
+        let res = -1; 
+        
+        for (let i  = 0; i < n; i++) {
+            let j = 0;
+            let leftSum = 0;
+            let rightSum = 0; 
+
+            while(j < i) {
+                leftSum += nums[j++];
+            }
+
+            j = i + 1;
+
+            while(j < n) {
+                rightSum += nums[j++];
+            }
+            if (rightSum == leftSum) {
+                return i; 
+            }
+        }
+        return res;
+};
