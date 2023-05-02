dcaa470707825c34688f96f18aacf3ca9302c831
ahmed <ahmadghnnam60@gmail.com>
2023-05-02 22:38:07 +0300

    added a JavaScript solution of Solve the Equation

---
+++ 640.Solve_the_Equation/640.Solve_the_Equation.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} equation
+ * @return {string}
+ */
+var solveEquation = function(equation) {
+    let sides = equation.split("=");
+      
+        let res = evaluate(sides[0]),
+        res1 = evaluate(sides[1]);
+
+
+        res[0] -= res1[0];
+        res[1] = res1[1] - res[1];
+
+        if(res[0] === 0 && res[1] === 0)
+            return  "Infinite solutions";
+        if(res[0] === 0) {
+            return "No solution";
+        } 
+        return "x=" + res[1] / res[0]; 
+};
+
+
+function evaluate(exp) {
+       
+    let tokens = exp.split("?=[-+]"); 
+    let res = [0, 0];
+    for (const token of tokens) {
+        if (token === "+x" || token === "x") 
+            res[0] += 1;
+	    else if (token === "-x") res[0] -= 1;
+	    else if (token.includes("x")) res[0] += parseInt(token.substring(0, token.indexOf("x")));
+	    else res[1] += parseInt(token);
+    }
+        return res;
+}
+
