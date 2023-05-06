6fb306d548b43aeff79f3eb50fa3ef726236b521
ahmed <ahmadghnnam60@gmail.com>
2023-05-06 10:54:37 +0300

    added a JavaScript solution of Decode XORed Permutation

---
+++ 1734.Decode_XORed_Permutation/1734.Decode_XORed_Permutation.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} encoded
+ * @return {number[]}
+ */
+var decode = function(encoded) {
+           
+        let n = encoded.length + 1;
+        let perm = [];
+        let x = 0;
+        for (let i = 1; i <= n; i++) 
+            x ^= i;
+
+        perm[0] = x;
+        for (let i = 1; i < n; i++) {
+            if (i % 2 == 1)
+                perm[0] ^= encoded[i];  
+        }
+
+
+        for (let i = 1; i < n; i++) {
+            perm[i] = encoded[i - 1] ^ perm[i - 1];  
+        }
+        return perm;
+
+};
