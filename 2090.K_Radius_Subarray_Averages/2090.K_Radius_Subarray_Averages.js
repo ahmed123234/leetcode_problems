96cd6aa7a57deb089e88556f22ea77b5c6d35138
ahmed <ahmadghnnam60@gmail.com>
2023-05-29 13:11:08 +0300

    added a JavaScript solution of K Raduis Subarray Averages

---
+++ 2090.K_Radius_Subarray_Averages/2090.K_Radius_Subarray_Averages.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number[]}
+ */
+var getAverages = function(nums, k) {
+    const n = nums.length;
+    const count = 2 * k + 1;
+
+    const res = [];
+    const prefixSum = [];
+       
+    for (let i = 0; i < n; i++) {
+        if (i == 0) {
+            prefixSum.push(nums[i]);    
+        } else
+            prefixSum.push(prefixSum[i - 1] + nums[i]);
+    }
+
+    for (let i = 0; i < n; i++) {
+        if (i >= k && n - i - 1>= k) {
+            let j = i - k;
+            let sum = 0;
+
+            if (i - k === 0) {
+                sum = prefixSum[i + k]; 
+            } else
+                sum = prefixSum[i + k] -
+            prefixSum[i - k - 1];
+
+            res.push(Math.floor(sum / count));
+        } else {
+            res.push(-1);
+        }
+    }
+    return res;
+};
