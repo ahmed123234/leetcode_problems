0f156e91184077ca3392c82e70793d037d3158ce
ahmed <ahmadghnnam60@gmail.com>
2023-05-30 10:40:37 +0300

    added a JavaScript solution of Maximum Average Subarray I

---
+++ 643.Maximum_Average_Subarray_I/643.Maximum_Average_Subarray_I.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findMaxAverage = function(nums, k) {
+     // using sliding window pattern 
+
+        let windowSum = 0;
+        let start = 0;
+        let maxAvg = - Infinity;
+        const n = nums.length;
+
+        for (let i = 0; i < n; i++) {
+            windowSum += nums[i];
+
+            if(i - start + 1 == k) {
+                const avg = (windowSum / k) ;
+                maxAvg = Math.max(maxAvg, avg);
+                windowSum -= nums[start];
+                start += 1;
+            }
+        }
+        return maxAvg;
+};
