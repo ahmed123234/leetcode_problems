a6ec4e9e4b07d9a67431e12caafc9de9c2d21dbb
ahmed <ahmadghnnam60@gmail.com>
2023-05-30 09:23:25 +0300

    added a JavaScript solution of Removing Minimum and Maximum From Array

---
+++ 2091.Removing_Minimum_and_Maximum_From_Array/2091.Removing_Minimum_and_Maximum_From_Array.js
@@ -0,0 +1,44 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var minimumDeletions = function(nums) {
+    const n = nums.length;
+
+    let minF = nums[0];
+    let maxF = nums[0];    
+    
+    let minB = nums[n - 1];
+    let maxB = nums[n - 1]
+
+    let minCf = 1, minCB = 1, maxCf = 1, maxCB = 1;
+
+    for (let i = 0, j = n - 1; i < n && j >= 0; i++,j--) {
+        if (nums[i] > maxF) {
+            maxF = nums[i];
+            maxCf = i + 1;
+        }
+
+        if (nums[j] > maxB) {
+            maxB = nums[j];
+            maxCB = n - j;
+        }
+
+        if (nums[i] < minF) {
+            minF = nums[i];
+            minCf = i + 1;
+        }
+
+        if (nums[j] < minB) {
+            minB = nums[j];
+            minCB = n - j;
+        }
+    }
+
+    return Math.min(
+        Math.max(minCf , maxCf),
+        (Math.min(minCB, minCf) + Math.min(maxCB, maxCf)),
+        Math.max(minCB , maxCB)
+    )
+
+};
