b93b8018827d0c0f89506a36f43055198f7df8c0
ahmed <ahmadghnnam60@gmail.com>
2023-05-31 07:59:48 +0300

    added a JavaScript solution of Minimum Recolors to Get K Consecutive Black Blocks

---
+++ 2379.Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks/2379.Minimum_Recolors_to_Get_K_Consecutive_Black_Blocks.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} blocks
+ * @param {number} k
+ * @return {number}
+ */
+var minimumRecolors = function(blocks, k) {
+    let windowCount = 0;
+    let ans = + Infinity;
+    let start = 0;
+    let n = blocks.length;
+
+    for(let i = 0; i < n; i++) {
+        if (blocks.charAt(i) == 'W')
+            windowCount ++;
+        
+        if (i - start + 1 == k) {
+            ans = Math.min(ans, windowCount);
+
+            if (blocks.charAt(start) == 'W')
+                windowCount --;
+
+            start ++;
+        }
+    }
+    return ans;
+};
