992680a6c9169f165cc1e2d51bb4ddc401ed9ceb
ahmed <ahmadghnnam60@gmail.com>
2023-06-01 09:35:15 +0300

    added a JavaScript solution of Find Maximum Number of Vowels in a Substring of Given Length

---
+++ 1456.Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length/1456.Maximum_Number_of_Vowels_in_a_Substring_of_Given_Length.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {number}
+ */
+var maxVowels = function(s, k) {
+    let slidingCount = 0;
+    let n = s.length;
+    let max = 0;
+    let start = 0;
+
+    for (let i = 0; i < n; i++) {
+        
+        if (isVowel(s.charAt(i))) {
+            slidingCount ++;
+        }
+
+        if (i - start + 1 === k) {
+            max = Math.max(max, slidingCount);
+            if(isVowel(s.charAt(start))) {
+                slidingCount --;
+            }
+            start ++;
+        }
+
+    }
+    return max;
+};
+
+function isVowel(c) {
+    if (c === 'a' || c === 'e' || c === 'i'|| c === 'o' || c === 'u') 
+    return true;
+
+    return false;
+}
