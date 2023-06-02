72ca6fc713365c6c0903257aa3e0923ea93236f2
ahmed <ahmadghnnam60@gmail.com>
2023-06-02 09:20:52 +0300

    added a JavaScript solution of Length of the Longest Alphapetical Continuous Subtring

---
+++ 2414.Length_of_the_Longest_Alphabetical_Continuous_Substring/2414.Length_of_the_Longest_Alphabetical_Continuous_Substring.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var longestContinuousSubstring = function(s) {
+     let start = 0;
+    let max = 0;
+    let slidingLength = 0;
+    let n = s.length;
+    let alphabetical = "abcdefghijklmnopqrstuvwxyz";
+    for(let i = 0; i< n; i++) {
+        const str = s.substring(start, i+ 1);
+
+        if(alphabetical.includes(str)) {
+            slidingLength = str.length;
+            max = Math.max(max, slidingLength);
+        } else {
+            start ++;
+        }
+    }
+    return max;
+};
