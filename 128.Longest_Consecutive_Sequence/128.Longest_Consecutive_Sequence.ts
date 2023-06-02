91d0fe82c750402b12ac2d6199d940a9f6284703
ahmed <ahmadghnnam60@gmail.com>
2023-06-02 09:15:32 +0300

    added a TypeScript solution of Longest Consuctive Sequence

---
+++ 128.Longest_Consecutive_Sequence/128.Longest_Consecutive_Sequence.ts
@@ -0,0 +1,29 @@
+function longestConsecutive(nums: number[]): number {
+    const map = new Map();
+    let max = 0;
+    let n = nums.length;
+
+    for (let num of nums) {
+        map.set(num, false);
+    }
+
+    // determine the starting point element
+    for (let i = 0; i < n; i++) {
+        if (!map.has(nums[i] - 1))  
+            map.set(nums[i], true);
+    }   
+
+    for (let i = 0; i < n; i++) {
+        if (map.get(nums[i]) === true) {
+            let j = 0;
+            let count = 0;
+            while(map.has(nums[i] + j)) {
+                count ++;
+                j++;
+            }
+            max = Math.max(max, count);
+        } 
+    } 
+
+    return max;
+};
