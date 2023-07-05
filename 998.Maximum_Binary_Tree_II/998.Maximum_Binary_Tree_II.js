8a34a560ba36898f092c660b9ba21b6f124c4f95
ahmed <ahmadghnnam60@gmail.com>
2023-07-05 07:48:50 +0300

    added a JavaScript solution of Maximum Binary Tree II

---
+++ 998.Maximum_Binary_Tree_II/998.Maximum_Binary_Tree_II.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} val
+ * @return {TreeNode}
+ */
+var insertIntoMaxTree = function(root, val) {
+    if (root !== null && root.val > val) {
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+
+    const node = new TreeNode(val);
+    node.left = root;
+
+    return node;
+};
